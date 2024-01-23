518f6b3112561aa754446bfdf748b68c99378705
Melkri <michal.bryzik13@gmail.com>
2024-01-23 11:42:59 +0100

    3_Feature adjusted

--- 3_Feature_Selection.R
+++ 3_Feature_Selection.R
@@ -33,7 +33,14 @@ importance <- varImp(model, scale=FALSE)
 print(importance)
 plot(importance)
 
-control2 <- rfeControl(functions=rfFuncs, method="cv", number=10, verboseIter = TRUE)
-results <- rfe(df_train[,c(1:2, seq(4:length(df_train)))], df_train[,3], sizes=c(1:17), rfeControl=control2)
+control2 <- rfeControl(functions=rfFuncs, method="cv", number=4)
+results <- rfe(df_train[, c(1:2, seq(4, ncol(df_train)))], as.matrix(df_train[,3]), sizes=c(1:22), rfeControl=control2)
+
 predictors(results)
 plot(results, type=c("g", "o"))
+
+recipe_obj <- recipe(fare_amount~ ., data = df_train) %>%
+  step_scale(all_predictors()) %>%  # Standardize the predictors
+  step_pca(all_predictors(), threshold = 0.96)  # Perform PCA with 90% variance threshold
+
+df_train_pca <- bake(prep(recipe_obj), new_data = df_train)
